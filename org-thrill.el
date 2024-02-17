b0c87317707678f77b26bf7810593b21e22e4643
Siyuan Wang <c.one@thrimbda.com>
2024-02-18 00:27:01 +0800

    fix: org agenda list args

--- org-thrill.el
+++ org-thrill.el
@@ -1411,9 +1411,10 @@ so change the default 'F' binding in the agenda to allow both"
 (setq org-agenda-start-on-weekday 1)
 
 ;; Enable display of the time grid so we can see the marker for the current time
+;; ISSUE: fix org agenda list args
 (setq org-agenda-time-grid (quote ((daily today remove-match)
-                                   #("----------------" 0 16 (org-heading t))
-                                   (0900 1100 1300 1500 1700))))
+                                   (0900 1100 1300 1500 1700 1900 2100)
+                                   "......" "----------------")))
 
 ;; Display tags farther right
 (setq org-agenda-tags-column -102)
