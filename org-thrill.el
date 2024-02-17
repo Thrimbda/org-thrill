2f08a688ff2cd1c2f89808686b953c646489094f
Siyuan Wang <c.one@thrimbda.com>
2024-02-18 01:13:08 +0800

    chore: revert org agenda time grid

--- org-thrill.el
+++ org-thrill.el
@@ -1413,8 +1413,8 @@ so change the default 'F' binding in the agenda to allow both"
 ;; Enable display of the time grid so we can see the marker for the current time
 ;; ISSUE: fix org agenda list args
 (setq org-agenda-time-grid (quote ((daily today remove-match)
-                                   (0900 1100 1300 1500 1700 1900 2100)
-                                   "......" "----------------")))
+                                   #("----------------" 0 16 (org-heading t))
+                                   (0900 1100 1300 1500 1700 1900 2100))))
 
 ;; Display tags farther right
 (setq org-agenda-tags-column -102)
@@ -1554,6 +1554,7 @@ Late deadlines first, then scheduled, then non-late deadlines"
 (setq org-clock-sound "/usr/local/lib/tngchime.wav")
 
 ; Enable habit tracking (and a bunch of other modules)
+;; ISSUE: comment out unused modules
 (setq org-modules (quote (
                           ;; org-bbdb
                           ;; org-bibtex
